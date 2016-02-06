ee4f0b4a6d74890bdc9c34cef2ef91df2b6ab442
Tony Kalou <cr4shmaster@hotmail.com>
2016-02-06 16:02:42 +0200

    SW update

--- modinfo.lua
+++ modinfo.lua
@@ -1,13 +1,19 @@
-name = "Magical Pouch"
+name = "Magical Pouch TEST"
 description = "Shrinks items to fit in your pocket!"
 author = "cr4shmaster"
-version = "3.0.5"
+version = "3.1.0"
 forumthread = ""
 api_version = 6
 dont_starve_compatible = true
 reign_of_giants_compatible = true
+shipwrecked_compatible = true
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
+
+--[[ 
+Notes:
+ - need to implement shipwrecked items
+--]]
 
 local crsSize = {
  {description = "4 slots", data = 1},
